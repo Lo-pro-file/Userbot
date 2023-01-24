74cb069d8d550a4413822e91d016f92298f8e0b7
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-24 21:03:32 +0530

    op

--- AdityaHalder/plugins/addall.py
+++ AdityaHalder/plugins/addall.py
@@ -30,7 +30,7 @@ async def inviteall(client: Client, message: Message):
 
 __MODULE__ = "Aᴅᴅ Aʟʟ"
 __HELP__ = f"""
-`.addall [@groupusername]` **- Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ Tᴏ Aᴅᴅ Mᴇᴍʙᴇʀs Iɴ Yᴏᴜʀ Cʜᴀᴛ**
+`.addall [@WOODcraft_Mirror_Topic]` **- Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ Tᴏ Aᴅᴅ Mᴇᴍʙᴇʀs Iɴ Yᴏᴜʀ Cʜᴀᴛ**
 
-**Ex:-** `.addall @adityadiscus`
+**Ex:-** `.addall @WOODcraft_Mirror_Topic`
 """
